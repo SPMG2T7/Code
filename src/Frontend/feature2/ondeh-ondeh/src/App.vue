c9c5524c819dfdf7852493cd6e7fe82b9bc28458
kohchloe <chloekoh.2021@scis.smu.edu.sg>
2023-09-19 14:55:32 +0800

    colour change

--- src/Frontend/feature2/ondeh-ondeh/src/App.vue
+++ src/Frontend/feature2/ondeh-ondeh/src/App.vue
@@ -20,7 +20,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #379462;
   margin-top: 60px;
 }
 </style>
