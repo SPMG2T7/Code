25a0d35b896c514cad21e38f12a74f45a4cad196
jxff_so <jeffso12@hotmail.com>
2023-09-21 15:40:08 +0800

    remodeled db script based on changes

--- src/Backend/prepare_script.sql
+++ src/Backend/prepare_script.sql
@@ -3,54 +3,55 @@ USE spm_g2t7;
 
 CREATE TABLE staff (
 	staff_id INT PRIMARY KEY NOT NULL,
-    staff_fname VARCHAR(50) NOT NULL,
-    staff_lname VARCHAR(50) NOT NULL,
-    dept VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    department VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     access_rights INT NOT NULL,
     FOREIGN KEY (access_rights) REFERENCES access_rights(access_id)
 );
 
 CREATE TABLE access_rights (
-	access_id INT PRIMARY KEY NOT NULL,
+	access_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     type VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE role (
-	role_id INT AUTO_INCREMENT NOT NULL,
+	role_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	role_name VARCHAR(20) NOT NULL,
-    role_desc LONGTEXT NOT NULL,
-    PRIMARY KEY (role_id, role_name)
+    role_description LONGTEXT NOT NULL,
+    listed_by INT NOT NULL,
+    expiry_timestamp INT NOT NULL 
 );
 
 CREATE TABLE skill (
 	skill_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	skill_name VARCHAR(50) NOT NULL,
-    skill_desc LONGTEXT NOT NULL
+    skill_description LONGTEXT NOT NULL
 );
 
 CREATE TABLE role_skill (
-    skill_name VARCHAR(50) NOT NULL ,
-	role_name VARCHAR(20) NOT NULL,
-    PRIMARY KEY (skill_name, role_name),
-    FOREIGN KEY (role_name) REFERENCES role(role_name)
+    role_id INT NOT NULL,
+	skill_id INT NOT NULL,
+    PRIMARY KEY (role_id, skill_id),
+    FOREIGN KEY (role_id) REFERENCES role(role_id)
 );
 
 CREATE TABLE staff_skill (
 	staff_id INT NOT NULL,
-    skill_name VARCHAR(50) NOT NULL,
-    PRIMARY KEY (staff_id, skill_name),
+    skill_id INT NOT NULL,
+    PRIMARY KEY (staff_id, skill_id),
     FOREIGN KEY (staff_id) REFERENCES staff(staff_id),
-    FOREIGN KEY (skill_name) REFERENCES skill(skill_name)
+    FOREIGN KEY (skill_id) REFERENCES skill(skill_id)
 );
 
 CREATE TABLE role_applicant (
-	role_name VARCHAR(20),
-    staff_id INT,
+	role_id INT NOT NULL,
+    staff_id INT NOT NULL,
     comments LONGTEXT NULL,
-    creation_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    PRIMARY KEY (role_name, staff_id),
-    FOREIGN KEY (role_name) REFERENCES role(role_name),
+    creation_timestamp INT NOT NULL,
+    PRIMARY KEY (role_id, staff_id),
+    FOREIGN KEY (role_id) REFERENCES role(role_id),
     FOREIGN KEY (staff_id) REFERENCES staff(staff_id)
 );
 
@@ -71,11 +72,11 @@ INSERT INTO staff VALUES
 (00123460, 'Gallywix', 'Howdy','Admin','gallywixh@aio.com', '1');
 
 INSERT INTO role VALUES
-(00001, 'Account Manager', 'Handle customer account under the Sales Division'),
-(00002, 'Support Team', 'Provide IT support under the System Solutioning Division'),
-(00003, 'Junior Engineer', 'Junior Software Engineer under the Engineering Division'),
-(00004, 'Senior Engineer', 'Senior Software Engineer under the Engineering Division'),
-(00005, 'Finance Executive', 'Assist with payroll-related matters under the Finance Division');
+(00001, 'Account Manager', 'Handle customer account under the Sales Division', '00123459', 1701388800),
+(00002, 'Support Team', 'Provide IT support under the System Solutioning Division', '00123459', 1701388800),
+(00003, 'Junior Engineer', 'Junior Software Engineer under the Engineering Division', '00123459', 1701388800),
+(00004, 'Senior Engineer', 'Senior Software Engineer under the Engineering Division', '00123459', 1701388800),
+(00005, 'Finance Executive', 'Assist with payroll-related matters under the Finance Division', '00123459', 1701388800);
 
 INSERT INTO skill VALUES 
 (00001, 'Microsoft Excel', 'Comfortable with creating and debugging simple Macro'),
@@ -104,8 +105,8 @@ INSERT INTO role_skill VALUES
 (00005, 00002),
 (00005, 00004);
 
-INSERT INTO role_applicant (role_name, staff_id, comments) VALUES 
-(00003, 00123457, 'I have always had a deep passion in software engineering. During my free time I have taken up multiple software-related certifications.'),
-(00004, 00123452, 'While I may not have the necessary skillset at the moment, I am very passionate to learn.'),
-(00005, 00123458, 'Finance is my passion'),
-(00002, 00123456, 'Interested in the Sales Division');
+INSERT INTO role_applicant VALUES 
+(00003, 00123457, 'I have always had a deep passion in software engineering. During my free time I have taken up multiple software-related certifications.', 1695174041),
+(00004, 00123452, 'While I may not have the necessary skillset at the moment, I am very passionate to learn.', 1695174041),
+(00005, 00123458, 'Finance is my passion', 1695174041),
+(00002, 00123456, 'Interested in the Sales Division', 1695174041);
