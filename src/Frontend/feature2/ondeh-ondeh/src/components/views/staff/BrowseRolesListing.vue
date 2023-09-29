14dce7c624d1249bb1b0e8380e3a95744728d534
kohchloe <chloekoh.2021@scis.smu.edu.sg>
2023-09-29 10:43:52 +0800

    Staff Browse Role Listings page

--- src/Frontend/feature2/ondeh-ondeh/src/components/views/staff/BrowseRolesListing.vue
+++ src/Frontend/feature2/ondeh-ondeh/src/components/views/staff/BrowseRolesListing.vue
@@ -1,29 +1,71 @@
 <template>
-    <h1>HELLO</h1>
-    <router-link to="/">Home</router-link>
-    <router-link to="/RoleCreation">Role Creation</router-link>
-    <RouterView>
-        
-    </RouterView>
+    <div>
+        <Nav/>
+    </div>
+    
+    <div>
+        <h1>Role List</h1>
+        <!-- v-if here means the v-for below will only run if the length of roles.length is not 0 -->
+        <ul v-if="roles.length">
+            <!-- What this does is to create a <li> for each entry of role that it finds in the db
+                e.g. if there are five entries in the DB, it will create this same <li> five times
+                    think of it as template -->
+            <li v-for="role in roles" :key="role.role_id">
+                <h2>{{ role.role_name }}</h2>
+                <p>Department: {{ role.department }}</p>
+                <p>Location: {{ role.location }}</p>
+                <p>Role Description: {{ role.role_description }}</p>
+                <!-- Add other properties as needed -->
+            </li>
+        </ul>
+        <!-- if the number of entries is 0, v-else will run -->
+        <p v-else>No roles available</p>
+    </div>
+
 </template>
   
 <script>
-// import HelloWorld from "../../HelloWorld.vue"
+import Nav from "../../views/NavBar.vue"
+import axios from 'axios'
 
-// export default {
-//     name: 'App',
-//     components: {
-//         // HelloWorld
-//     }
-// }
+export default {
+    name: "App",
+    components: {
+        Nav
+    },
+    // we will initialise an empty list here first so we can append the axios return later
+    data() {
+        return {
+            roles: []
+        };
+    },  
+    // think of this as calling the function right when u load the page
+    mounted() {
+        this.fetchRoles();
+    },
+    methods: {
+        // the function that helps us call the endpoint and retrieve the data
+        fetchRoles() {
+            const apiUrl = 'http://127.0.0.1:5000/roles/get_all';
+            axios.get(apiUrl)
+                .then(response => {
+                    this.roles = response.data.data.roles;
+                })
+                .catch(error => {
+                    console.error('Error fetching roles:', error);
+                });
+        }
+    }
+};
 </script>
   
 <style>
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
-    /* -webkit-font-smoothing: antialiased; */
-    /* -moz-osx-font-smoothing: grayscale; */
-    /* text-align: center; */
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #af8ece;
+    margin-top: 60px;
 }
 </style>
-  
