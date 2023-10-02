411a2637b8c08e8e96e94ba581d7ab55ab5f6d95
Keith Chan <keithchanyk28.17@ichat.sp.edu.sg>
2023-10-03 00:49:19 +0800

    applicant-role skills match table

--- src/Frontend/feature2/ondeh-ondeh/src/components/views/hr/IndivApplicant.vue
+++ src/Frontend/feature2/ondeh-ondeh/src/components/views/hr/IndivApplicant.vue
@@ -13,9 +13,12 @@ export default {
             staffName: "",
             currentRole: "",
             staffSkills: [],
+            roleSkills: [],
+            unmatchedSkills: [],
             appliedRole: ""
         };
     },
+
     // computed: {
     //     getID() {
     //         return this.$route.query.id
@@ -32,8 +35,16 @@ export default {
                     this.responseData_staffData = response.data.data.staff_data;
                     this.staffName = this.responseData_staffData.first_name + " " + this.responseData_staffData.last_name;
                     this.staffSkills = this.responseData_staffData.staff_skills
+                    this.roleSkills = this.responseData_roleData.skills_required
                     this.currentRole = this.responseData_staffData.current_role
                     this.appliedRole = this.responseData_roleData.role_name
+
+
+                    // Calculate unmatchedSkills here after fetching data
+                    this.unmatchedSkills = this.staffSkills.filter(
+                        (skill) => !this.roleSkills.includes(skill)
+                    );
+
                     console.log(this.responseData_roleData)
                     console.log(this.responseData_staffData)
                 })
@@ -42,8 +53,17 @@ export default {
                 });
         },
     },
+    computed: {
+        percentageMatchingSkills() {
+            const matchingSkills = this.roleSkills.filter(skill => this.staffSkills.includes(skill));
+            const percentage = (matchingSkills.length / this.roleSkills.length) * 100;
+            return percentage; // Limit the percentage to two decimal places
+        }
+    },
     mounted: function () {
         this.fetchData();
+
+        console.log(this.unmatchedSkills)
     }
 
 }
@@ -51,18 +71,49 @@ export default {
 
 <template>
     <Nav />
-    <h1>Individual Applicant</h1>
+
     <h2>{{ staffName }}</h2>
-    <p>Current Job Role: {{ currentRole }}</p>
-    <p>Applied for Job Role: {{  appliedRole }}</p>
-    <p>Skills: </p>
+    <p><span class="fw-bold">Current Job Role:&emsp;</span>{{ currentRole }}</p>
+    <p><span class='fw-bold'>Applied for Job Role:&emsp;</span>{{ appliedRole }}</p>
+    <p><span class="fw-bold">Skill Match - </span>{{ percentageMatchingSkills }}%</p>
+    <table class="table w-auto">
+        <thead>
+            <tr>
+                <th scope="col" class="fw-bold">Skill Name</th>
+                <th scope="col" class="fw-bold">Role Requirement</th>
+                <th scope="col" class="fw-bold">My Skill</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="rSkills in roleSkills" :key="rSkills"
+                :class="{ 'table-success': staffSkills.includes(rSkills), 'table-danger': !staffSkills.includes(rSkills) }">
+                <td>{{ rSkills }}</td>
+                <td>✓</td>
+                <td v-if="staffSkills.includes(rSkills)">✓</td>
+                <td v-else>✗</td>
+                <!-- <td>Otto</td>
+                <td>{{ skill }}</td> -->
+            </tr>
+        </tbody>
+    </table>
+
+    <p><span class="fw-bold">Other Skills: </span> {{ unmatchedSkills.join(', ') }}</p>
+
+
+
+    <!-- <p>Staff Skills: </p>
     <ul>
         <li v-for="skill in staffSkills" :key="skill">{{ skill }}</li>
     </ul>
+
+    <p>Role Skills: </p>
+    <ul>
+        <li v-for="rSkills in roleSkills" :key="rSkills">{{ rSkills }}</li>
+    </ul> -->
 </template>
 
 <style scoped>
-* {
+/* * {
     padding-left: 40px;
-}
+} */
 </style>
