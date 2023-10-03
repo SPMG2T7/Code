987bb6442be86ff9af29f6fd95b0b753e4d147da
kohchloe <chloekoh.2021@scis.smu.edu.sg>
2023-10-03 18:26:41 +0800

    Update BrowseRolesListing.vue

--- src/Frontend/feature2/ondeh-ondeh/src/components/views/staff/BrowseRolesListing.vue
+++ src/Frontend/feature2/ondeh-ondeh/src/components/views/staff/BrowseRolesListing.vue
@@ -4,18 +4,25 @@
     </div>
     
     <div>
-        <h1>Role List</h1>
         <!-- v-if here means the v-for below will only run if the length of roles.length is not 0 -->
-        <ul v-if="roles.length">
+        <ul class="role-list" v-if="roles.length">
+            
             <!-- What this does is to create a <li> for each entry of role that it finds in the db
                 e.g. if there are five entries in the DB, it will create this same <li> five times
                     think of it as template -->
             <li v-for="role in roles" :key="role.role_id">
-                <h2>{{ role.role_name }}</h2>
-                <p>Department: {{ role.department }}</p>
-                <p>Location: {{ role.location }}</p>
-                <p>Role Description: {{ role.role_description }}</p>
-                <!-- Add other properties as needed -->
+
+                <div class="container-fluid" style="width: 100%; margin: 30px 0px; border: 1px solid black">
+                    <div class="row">
+                    
+                        <h3>{{ role.role_name }}</h3>
+                        <p>{{ role.no_of_pax }} staff needed</p>
+                        <p>Closing in {{ role.days_left }} days</p>
+                        <button type="apply" class="btn btn-secondary" onclick="apply()">Apply</button>
+                        
+                    </div>
+                </div>
+
             </li>
         </ul>
         <!-- if the number of entries is 0, v-else will run -->
@@ -59,13 +66,24 @@ export default {
 };
 </script>
   
-<style>
+<style scoped>
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #af8ece;
+    color: #000000;
     margin-top: 60px;
+}
+
+h3 {
+    font-size: 20px;
+    font-weight: bold;
+}
+
+.role-list {
+    list-style: none; /* Remove bullet points */
+    padding: 0; /* Remove default padding */
+    margin: 0; /* Remove default margin */
 }
 </style>
