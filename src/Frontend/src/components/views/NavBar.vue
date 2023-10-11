87ddf4750858e7b5a7fe916a0db172877d50b595
minnalk <94851324+minnalk@users.noreply.github.com>
2023-10-11 21:38:39 +0800

    Updated NavBar

--- src/Frontend/src/components/views/NavBar.vue
+++ src/Frontend/src/components/views/NavBar.vue
@@ -29,6 +29,28 @@ export default {
                     console.error('Error:', error);
                 });
         },
+
+        resetSession() {
+            sessionStorage.clear();
+            location.reload();
+        },
+
+        redirectBrowse() {
+            window.location.assign('/');
+        },
+
+        redirectCreate() {
+            window.location.assign('/RoleCreation');
+        },
+
+        redirectProfile() {
+            window.location.assign('/Profile');
+        },
+
+        redirectSettings() {
+            window.location.assign('/Settings');
+        }
+
     },
     mounted: function () {
         this.fetchData();
@@ -43,51 +65,36 @@ export default {
 </script>
 
 <template>
-    <!-- START OF REFERENCE NAVBAR -->
-    <!-- <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav> -->
-    <!-- END OF REFERENCE NAV BAR -->
 
     <nav class="navbar navbar-expand-lg bg-body-tertiary rounded-3">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">GoJobs Portal</a>
+            <img class="mx-2" src="../../assets/gojobs_logo.png" />
+            <span class="navbar-brand fw-bold">GoJobs Portal</span>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                <ul class="navbar-nav ms-left mb-2 mb-lg-0">
+
+                    <div class="nav-link p-0" data-bs-toggle="dropdown">
+                        <li class="nav-item profile-name">
+                            <button class="btn btn-default" style="margin:0px 10px; background: rgba(233, 196, 220, 0.3)">
+                                <span>Navigate To</span>
+                            </button>
+
+                        <div class="dropdown-menu dropdown-menu-start" aria-labelledby="dropdownMenuButton">
+                            <span class="dropdown-item" @click="redirectBrowse()">Browse Role Listings</span>
+                            <span v-if="this.accessId == 2" class="dropdown-item" @click="redirectCreate()">+ Create Role Listing</span>
+                        </div>
+
+                    </li>
+            </div>
+            </ul>
+
+
+
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item profile-name">
                         <!-- Welcome, {{ name }} ({{ balance_points }} Points) -->
@@ -101,19 +108,11 @@ export default {
                             </button>
 
 
-                            <div class="dropdown-menu dropdown-menu-end">
-                        <!-- <li v-if="this.cid == 9999 || this.cid == 9998" @click="generateCharts">
-                            <a class="dropdown-item">Dashboard</a>
-                        </li> -->
-                        <li><a class="dropdown-item">Settings</a>
-                        </li>
-
-                        <!-- LOG OUT BUTTON -->
-                        <!-- <div class="dropdown-divider"></div>
-                                <li @click="logout">
-                                    <a class="dropdown-item"> Log Out</a>
-                                </li> -->
-                    </div>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                            <span class="dropdown-item" @click="redirectProfile()">Profile</span>
+                            <span class="dropdown-item" @click="redirectSettings()">Settings</span>
+                            <span class="dropdown-item" @click="resetSession()" style="cursor:pointer;">Log Out</span>
+                        </div>
 
                     </li>
             </div>
