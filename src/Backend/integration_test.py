e5c06ce7ad63f4de03325e88811c1af7117cd863
Zong Wei Eng <engzongwei@hotmail.com>
2023-10-18 00:01:06 +0800

    Update integration_test.py

--- src/Backend/integration_test.py
+++ src/Backend/integration_test.py
@@ -21,6 +21,8 @@ class TestApp(flask_testing.TestCase):
 
 
 class TestGetAllStaff(TestApp):
+    # Testing for the endpoint /staff/get_all
+    
     def test_get_all_staff(self):
         access_rights1 = Access_Rights(access_id=1, type="Admin")
         access_rights2 = Access_Rights(access_id=2, type="User")
@@ -107,6 +109,59 @@ class TestGetAllStaff(TestApp):
             '/staff/get_all', content_type='application/json')
 
         self.assertEqual(response.json["message"], 'There are no staffs')
+
+class TestGetSpecificStaff(TestApp):
+    # Testing for the endpoint /staff/<int:staff_id>
+     
+    def test_get_specific_staff(self):
+        access_rights1 = Access_Rights(access_id=1, type="Admin")
+        access_rights2 = Access_Rights(access_id=2, type="User")
+        access_rights3 = Access_Rights(access_id=3, type="Manager")
+
+        db.session.add(access_rights1)
+        db.session.add(access_rights2)
+        db.session.add(access_rights3)
+        db.session.commit()
+
+        staff1 = Staff(staff_id=1, first_name="John", last_name="Tan",
+                       email="JohnTan@gmail.com", department="IT", current_role="Software Engineer",
+                       access_rights=1)
+        staff2 = Staff(staff_id=2, first_name="Tony", last_name="Lim",
+                       email="TonyLin@gmail.com", department="OT", current_role="Ops Engineer",
+                       access_rights=2)
+        staff3 = Staff(staff_id=3, first_name="Zong", last_name="Wei",
+                       email="ZongWei@gmail.com", department="IT", current_role="Software Junior",
+                       access_rights=2)
+        staff4 = Staff(staff_id=4, first_name="Poppy", last_name="Lim",
+                       email="Poppy@gmail.com", department="Sales", current_role="Sales manager",
+                       access_rights=3)
+        
+        db.session.add(staff1)
+        db.session.add(staff2)
+        db.session.add(staff3)
+        db.session.add(staff4)
+        db.session.commit()
+
+        response = self.client.get(
+            '/staff/1', content_type='application/json')
+        
+        self.assertEqual(response.json[0]['data'], {
+                'access_rights': 1,
+                'current_role': 'Software Engineer',
+                'department': 'IT',
+                'email': 'JohnTan@gmail.com',
+                'first_name': 'John',
+                'last_name': 'Tan',
+                'staff_id': 1,
+                'staff_skills': []
+            }
+        )
+
+    def test_get_specific_staff_not_found(self):
+        response = self.client.get(
+            '/staff/5', content_type='application/json')
+        
+        self.assertEqual(response.json["message"], 'Staff not found.')
 
 if __name__ == '__main__':
     unittest.main()
